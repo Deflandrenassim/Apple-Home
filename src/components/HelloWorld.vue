5138e718a1a15055334739beb75b6130ed78fe9a
Deflandrenassim <nassim.def@gmail.com>
2022-02-07 20:37:30 +0100

    add imgs caroussel

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,13 +1,22 @@
 <template>
   <div>
-    <h1> Hello World Welcome to NASS'PPLE</h1>
-
     <carousel>
       <slide>
-        
+        <img class="images" src="@/assets/imgs/ipadPro.png" alt="ipad pro " >
+      </slide>
+      <slide>
+        <img src="@/assets/imgs/iphone13pro.png" alt="ipad pro " >
+      </slide>
+      <slide>
+        <img src="@/assets/imgs/macbookproM1.png" alt="ipad pro " >
+      </slide>
+      <slide>
+        <img src="@/assets/imgs/watch7.png" alt="ipad pro " >
       </slide>
     </carousel>
     
+    <h1> {{ titre }}</h1>
+
   </div>
 </template>
 
@@ -18,8 +27,18 @@ import { Carousel, Slide } from 'vue-carousel';
     components : {
       Carousel,
       Slide
-
+    },
+    data: () =>{
+        return{
+          titre: 'Hello World Welcome to APPLE'
+        }
     }
 
   }
 </script>
+<style>
+.img{
+  height: 40px;
+  width: 40px;
+}
+</style>
