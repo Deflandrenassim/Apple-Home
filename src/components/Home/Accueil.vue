4f7ba36bc8e8d76c74ae8bbaf17c6d6bfda5b023
Deflandrenassim <nassim.def@gmail.com>
2022-02-08 19:00:52 +0100

    component cadeau

--- src/components/Home/Accueil.vue
+++ src/components/Home/Accueil.vue
@@ -36,10 +36,10 @@ export default {
   display: flex;
   flex-direction: column;
   align-items: center;
-  padding-top: 10px; 
+  padding: 19px; 
 }
 h1{
-  padding-top: 10px;
+  padding-top: 1Opx;
   display: flex;
   justify-content: center;
   text-transform: uppercase;
@@ -49,22 +49,24 @@ h1{
 }
 .price{
     font-size:25px;
+    padding: 10px;
 }
 
 .my-image {
-  height: 75vh;
+  height: 80vh;
   width: 80vh;
 }
+
 .btn{
     display: flex;
     justify-content: center;
     align-content: center;
-    height: 45px;
+    height: 40px;
     width: 70px;
     border-radius: 30px 30px ;
     background-color: #06c;
     color: white;
-   
+    
 }
 .btn:hover{
     background-color: rgb(4, 90, 177);
---
+++ src/components/Home/Cadeau.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+      <div class="background">
+
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'Cadeau'
+}
+</script>
+
+<style scoped>
+.background{
+    height: 100px;
+    width: 100%;
+    background: #34283c;
+}
+</style>
