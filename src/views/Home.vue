ce3d4d2838de432530e88fed02df6b7eb12bee4c
Deflandrenassim <nassim.def@gmail.com>
2022-02-10 08:03:45 +0100

    component home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,20 @@
 <template>
 <div>
   <Accueil />
-  <Cadeau />
+  
 </div>
 </template>
 
 <script>
 import Accueil from '../components/Home/Accueil.vue'
-import Cadeau from '../components/Home/Cadeau.vue'
+
 
   export default {
     name: 'Home',
 
     components: {
      Accueil,
-     Cadeau 
+      
    }
   }
 </script>
