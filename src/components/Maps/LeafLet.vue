2ad5cd5b676f7b9375c677076f3df147e87bd3b1
Deflandrenassim <nassim.def@gmail.com>
2022-02-21 12:51:25 +0100

    zoom and center map

--- src/components/Maps/LeafLet.vue
+++ src/components/Maps/LeafLet.vue
@@ -1,13 +1,17 @@
 <template>
+<div class="map">
   <l-map 
    :zoom="zoom" 
    :center="center">
     <l-tile-layer :url="url" ></l-tile-layer>
   </l-map>
+</div>
+ 
 </template>
 
 <script>
-import {LMap, LTileLayer,} from 'vue2-leaflet';
+import {LMap, LTileLayer,} from 'vue2-leaflet'
+import 'leaflet/dist/leaflet.css'
 
 export default {
   components: {
@@ -19,10 +23,16 @@ export default {
     return {
       url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
       
-      zoom: 10,
+      zoom: 1,
       center: [51.505, -0.159],
       markerLatLng: [51.504, -0.159]
     };
   }
 }
 </script>
+<style>
+.map{
+  height: 50%;
+  width: 50%;
+}
+</style>
