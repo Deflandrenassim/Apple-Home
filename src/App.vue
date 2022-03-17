0ecad9afb94507a43547a51be9d1c336d9d2c1c6
Deflandrenassim <nassim.def@gmail.com>
2022-03-17 20:31:00 +0100

    app

--- src/App.vue
+++ src/App.vue
@@ -1,77 +1,78 @@
 <template>
   <div>
-     <nav class="Navbar">
-       <i class=" fab fa-apple ">  </i>
-        <router-link to="/" class="choix"> <i class="fa-solid fa-house"></i>  </router-link> 
-        <router-link to="/Iphone" class="choix"> <i class="fa-solid fa-mobile-screen-button"></i> </router-link> 
-        <router-link to="/Ipad" class="choix"> <i class="fa-solid fa-tablet-screen-button"></i> </router-link> 
-        <router-link to="/Mac" class="choix"> <i class="fa-solid fa-desktop"></i> </router-link> 
-      
-        <hr>
-        
-      </nav>
+    <nav class="Navbar">
+      <i class="fab fa-apple"> </i>
+      <router-link to="/" class="choix">
+        <i class="fa-solid fa-house"></i>
+      </router-link>
+      <router-link to="/Iphone" class="choix">
+        <i class="fa-solid fa-mobile-screen-button"></i>
+      </router-link>
+      <router-link to="/Ipad" class="choix">
+        <i class="fa-solid fa-tablet-screen-button"></i>
+      </router-link>
+      <router-link to="/Mac" class="choix">
+        <i class="fa-solid fa-desktop"></i>
+      </router-link>
+
+      <hr />
+    </nav>
     <transition name="fade" mode="out-in">
-    <router-view/>
+      <router-view />
     </transition>
-    </div>
-  
+  </div>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
-
+  name: "App",
 };
 </script>
 
 <style>
-
-
-.Nav{
+.Nav {
   display: flex;
   justify-content: center;
-  
+
   flex-wrap: wrap;
   height: 5vh;
   background-color: rgb(250, 250, 255);
-  color : white;
+  color: white;
 }
-.Navbar{
+.Navbar {
   display: flex;
   justify-content: center;
   justify-content: space-evenly;
-  padding-right: 15PX;
-  background-color:rgb(51 51 51);
+  padding-right: 15px;
+  background-color: rgb(51 51 51);
   color: white;
   padding-top: 10px;
 }
-.choix{
+.choix {
   color: rgb(216, 214, 214);
   text-decoration: none;
   padding: 20px;
-
 }
-.choix:hover{
+.choix:hover {
   color: aliceblue;
 }
-.fa-apple{
+.fa-apple {
   padding-top: 20px;
   padding-right: 20px;
 }
-.fade-enter, .fade-leave-to {
+.fade-enter,
+.fade-leave-to {
   opacity: 0;
-  transform: translateX(2em)
+  transform: translateX(2em);
 }
-.fade-enter-active, .fade-leave-active{
-  transition: all .3s ease;
+.fade-enter-active,
+.fade-leave-active {
+  transition: all 0.3s ease;
 }
-.fa-solid, .fab {
+.fa-solid,
+.fab {
   font-size: 25px;
-
 }
-
-
 </style>
 
 
